2ae76cb03bca5f3b6f3a2fcc6011138e2ac4f276
Erik Edelbring <erik.edelbring@gmail.com>
2025-07-03 15:27:38 +0200

    Added benchmark tests and wait intervals for performance checks

--- board/board_test.go
+++ board/board_test.go
@@ -1,5 +1,4 @@
 // go test ./... runs tests in all sub packages
-
 package board
 
 import "testing"
@@ -9,5 +8,24 @@ func TestAlgebraicToCoords(t *testing.T) {
 	want := Coord{Row: 4, Col: 4}
 	if got != want {
 		t.Fatalf("e4: %+v, want: %+v", got, want)
+	}
+}
+
+func BenchmarkMoveGenerationStringSlice(b *testing.B) {
+	gs := NewGameState()
+	gs.Board = NewSliceBoard()
+
+	// This loop will run until the benchmark is satisfied
+	for b.Loop() {
+		_ = GenerateAllMoves(&gs)
+	}
+}
+
+func BenchmarkMoveGenerationBitboard(b *testing.B) {
+	gs := NewGameState()
+	gs.Board = NewSliceBoard() // func NewBitboardBoard and then test it against SliceBoard
+
+	for b.Loop() {
+		_ = GenerateAllMoves(&gs)
 	}
 }
--- main.go
+++ main.go
@@ -10,13 +10,14 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"time"
 )
 
 func main() {
 	// Start pprof server in the background, http://localhost:6060/debug/pprof/
 	// In terminal after started main.go in another terminal:
-	//  go tool pprof http://localhost:6060/debug/pprof/profile?seconds=30
-	// 	after 30 seconds you can run commands like: top, web(needs https://graphviz.org/download/)
+	//  go tool pprof http://localhost:6060/debug/pprof/profile?seconds=6
+	// 	after x seconds you can run commands like: top, web(needs https://graphviz.org/download/)
 	go func() {
 		log.Println("Starting pprof on http://localhost:6060/debug/pprof/")
 		err := http.ListenAndServe("localhost:6060", nil)
@@ -24,62 +25,65 @@ func main() {
 			log.Fatalf("pprof failed: %v", err)
 		}
 	}()
+
+	time.Sleep(3 * time.Second) // waits for things like performance checks
   
-  log.Println(http.ListenAndServe("localhost:6060", nil))
-  fmt.Println("Starting Chess Engine...")
-  gameState := board.FENtoGameState(board.GetStartingFEN())
-  board.PrintBoard(gameState.Board)
-  reader := bufio.NewReader(os.Stdin)
+	log.Println(http.ListenAndServe("localhost:6060", nil))
+	fmt.Println("Starting Chess Engine...")
+	gameState := board.FENtoGameState(board.GetStartingFEN())
+	board.PrintBoard(gameState.Board)
+	reader := bufio.NewReader(os.Stdin)
 
-  // Set to true if you want to play yourself
-  humanPlaying := false
-  if humanPlaying {
-    for {
-      allMoves := board.GenerateAllMoves(&gameState)
+	// Set to true if you want to play yourself
+	humanPlaying := false
+	if humanPlaying {
+		for {
+			allMoves := board.GenerateAllMoves(&gameState)
 
-      if board.CheckGameEnd(gameState.Results){
-        board.PrintResult(gameState.Results, gameState.WhiteToMove)
-        break
-      }
+			if board.CheckGameEnd(gameState.Results){
+				board.PrintResult(gameState.Results, gameState.WhiteToMove)
+				break
+			}
 
-      fmt.Print("Move (e.g. e2e4): ")
-      input, err := reader.ReadString('\n')
-      if err != nil {
-        fmt.Println("Error reading input:", err)
-        continue
-      }
+			fmt.Print("Move (e.g. e2e4): ")
+			input, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading input:", err)
+				continue
+			}
 
-      move := strings.TrimSpace(input)
+			move := strings.TrimSpace(input)
 
-      if move == "exit" {
-        break
-      }
+			if move == "exit" {
+				break
+			}
 
-      fmt.Println("You entered:", move)
+			fmt.Println("You entered:", move)
 
-      err = board.CheckHumanMove(&gameState, &allMoves, move)
-      if err != nil {
-        fmt.Println(err)
-        continue
-      }
+			err = board.CheckHumanMove(&gameState, &allMoves, move)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
 
-      board.PrintBoard(gameState.Board)
-    }
-  } else {
-    for {
-      allMoves := board.GenerateAllMoves(&gameState)
+			board.PrintBoard(gameState.Board)
+		}
+	} else {
+		for {
+			allMoves := board.GenerateAllMoves(&gameState)
 
-      if board.CheckGameEnd(gameState.Results){
-        board.PrintResult(gameState.Results, gameState.WhiteToMove)
-        break
-      }
+			if board.CheckGameEnd(gameState.Results){
+				board.PrintResult(gameState.Results, gameState.WhiteToMove)
+				break
+			}
 
-      move := allMoves[1] // takes the second move
+			move := allMoves[1] // takes the second move
 
-      board.ApplyMove(&gameState, move)
+			board.ApplyMove(&gameState, move)
 
-      board.PrintBoard(gameState.Board)
-      fmt.Println("_________________")
-    }
-  }
+			board.PrintBoard(gameState.Board)
+			fmt.Println("_________________")
+		}
+	}
+	//time.Sleep(20 * time.Second) // waits for things like performance checks
 }
